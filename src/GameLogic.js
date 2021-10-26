8bf8e3e22a95699acf63eb2a60dcdba80ad6d21b
Zachary <zvcoding@gmail.com>
2021-10-25 20:04:02 -0400

    With Random Generation

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,35 +1,68 @@
 import Generator from "./Generator.js"
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 
 function GameLogic() {
 
   const [userInput,setUserInput] = useState('')
   const [lastInput,setLastInput] = useState('')
   const [gamesInput, setGamesInput] = useState ('')
+  const [gamesInput2, setGamesInput2] = useState ('')
 
-  function setterOfGamesInput (e) {
-    console.log("goodbye")
+function gameFunction() {
+  if (userInput === gamesInput)
+  {console.log("correct")}
+  else {console.log("incorrect")}}
+
+
+gameFunction()
+
+
+  function handleChange(e){
+    setUserInput(e.target.value.slice(-1))
   }
 
 
+  function RandomLetterGenerator() {
+    let alphabetz = "abcdefghijklmnopqrstuvwxyz"
+    let randomLetterz = alphabetz[Math.floor(Math.random() * alphabetz.length)]
+    setGamesInput(randomLetterz);
+  }
 
+  function RandomLetterGeneratorz() {
+    let alphabetB = "abcdefghijklmnopqrstuvwxyz"
+    let randomLetterB = alphabetB[Math.floor(Math.random() * alphabetB.length)]
+    setGamesInput2(randomLetterB);
+  }
 
-function handleChange(e){
-console.log("Bye")
+  
+
+function timingFunction(){
+setInterval(()=>RandomLetterGenerator(), 2000)
+}
+useEffect(()=>timingFunction(),[])
+
+function secondTimingFunction(){
+  setInterval(()=>RandomLetterGeneratorz(), 3000)
 }
 
-function gameFunction() {
-  if (lastInput === gamesInput)
-  {console.log("correct")}
-   else {console.log("incorrect")}}
+useEffect(()=>secondTimingFunction(),[])
 
+
+
+console.log(`This is the User Input: ${userInput}`)
+console.log(`This is the Game Input: ${gamesInput}`)
+console.log(`This is the Game Input 2: ${gamesInput2}`)
+
+  
   
   return (
     <div>
+      {`This is ${gamesInput}`}
+      <p></p>
+      {`This is ${gamesInput2}`}
       <Generator gamesInput = {gamesInput} setGamesInput = {setGamesInput} />
-      <label>
-        Name:
-      <input type="text" name="name" onChange = {handleChange} />
+      <label> Input
+      <input type="text" name="name" onChange = {handleChange} value ={userInput} />
       </label>
       </div>
   );
--- src/Generator.js
+++ src/Generator.js
@@ -1,42 +1,13 @@
 import NumberGenerator from "./NumberGenerator"
 import RLG from "./RLG"
 import RCG from "./RCG";
-import React, { useEffect } from "react"
+import React from "react"
 
 
 function Generator({gamesInput, setGamesInput}) {
-
-function setterConsoleLog(e) {
-  console.log("hello ")
-}
-
-function pleaseWork () {
-  setInterval(setterConsoleLog, 5000)
-  
-}
-
-function setterOfGamesInput () {
-  // let alphabet = "abcdefghijklmnopqrstuvwxyz"
-  // let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
-  // console.log(randomLetter)
-  return <RLG />
-}
-useEffect(() => pleaseWork(), [])
-
-  // function grabRandomLetter () {
-    
-    // }
-    
-  
-
-  
   return (
     <div>
-      {/* {grabRandomLetter}  */}
-      {setterOfGamesInput()}
-    {/* <NumberGenerator /> */}
-    <RLG gamesInput = {gamesInput} setGamesInput = {setGamesInput} />
-    {/* <RCG /> */}
+        <RLG gamesInput = {gamesInput} setGamesInput = {setGamesInput} />
     </div>
   );
 }
