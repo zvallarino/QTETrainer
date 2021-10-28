2afb5b6ea36aad6d9dbec7f6ef578daca230fce7
Vida May Kotico <vmkotico@yahoo.com>
2021-10-28 00:45:28 -0700

    initial css edits

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Button } from 'semantic-ui-react'
 import HowTo from './HowTo';
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router, NavLink } from 'react-router-dom';
+import styled from 'styled-components';  
 
 function App() {
 
@@ -32,18 +33,24 @@ fetch("http://localhost:4000/leaderboard")
       <Route path ="/">
   
       <NavLink  to="/HowTo">
+        <HowToLinkStyle>
           <p>HOW TO</p>
+          </HowToLinkStyle>
       </NavLink>
-  
+
       <NavLink  to="/Start">
+        <StartStyle>
         <p>
           START 
           </p>
+          </StartStyle>
       </NavLink> 
       <NavLink  to="/leaderboard">
+        <LeaderBoardStyle>
           <p>
           LEADERBOARD
           </p>
+          </LeaderBoardStyle>
       </NavLink>  
       </Route>
       </Router>
@@ -54,3 +61,47 @@ fetch("http://localhost:4000/leaderboard")
 }
 
 export default App;
+
+const StartStyle = styled.div `
+background-color:  #f2d9d9;
+font-family: 'Games', sans-serif;
+font-size: 60px;
+  position: fixed;
+  top: 50%; 
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px; 
+  display: block;
+  height: 200px;
+  width: 300px;
+  border-radius: 50%;
+  border: 3px solid red;
+
+`
+
+const HowToLinkStyle = styled.div`
+  background-color: #f2d9d9;
+  font-family: 'Games', sans-serif;
+  font-size: 30px; 
+  position: absolute;
+  bottom:0; 
+  left:0; 
+  display: block;
+  height: 80px;
+  width: 180px;
+  border-radius: 50%;
+  border: 3px solid red;
+`
+const LeaderBoardStyle = styled.div`
+  background-color: #f2d9d9;
+  font-family: 'Games', sans-serif;
+  font-size: 25px;
+  position: absolute;
+  bottom:0; 
+  right:0; 
+  display: block;
+  height: 80px;
+  width: 180px;
+  border-radius: 50%;
+  border: 3px solid red;
+`
--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import styled from 'styled-components';  
+
+const StyledApp = styled.div`
+  min-height: 100vh; 
+  background-color: #bf4040
 
 
+`
 
-ReactDOM.render(
+ReactDOM.render( 
+  <StyledApp> 
   <Router> 
+  
     <App />
-  </Router>,
+    
+  </Router>
+  </StyledApp>,
   document.getElementById('root')
 );
 
@@ -18,3 +28,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
